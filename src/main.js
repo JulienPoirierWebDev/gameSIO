df2e97d12c9f33435b2b7fc94537d28fdd5a6c63
Julien POIRIER <connexion@julienpoirier-Webdev.com>
2025-01-13 12:18:04 +0100

    up

--- src/main.js
+++ src/main.js
@@ -1,24 +1,73 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import '/src/style.css';
 
-document.querySelector('#app').innerHTML = `
-  <div>cd
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const myH1 = document.querySelector('h1');
 
-setupCounter(document.querySelector('#counter'))
+console.log(myH1);
+
+myH1.textContent = 'Hahaha';
+myH1.classList.toggle('super-class');
+
+myH1.innerHTML = `<span> Je suis un element en inner</span><br><br><span>Moi aussi</span>`;
+
+myH1.addEventListener('click', function (event) {
+	if (myH1.style.backgroundColor === 'blue') {
+		myH1.style.backgroundColor = 'red';
+	} else {
+		myH1.style.backgroundColor = 'blue';
+	}
+});
+
+// selectionner le paragraphe
+// ajouter un évènement qui active du code quand on survol l'élement
+// Faire devenir plus gros le paragraphe
+
+// Etape 1 : selectionner les élements a manipuler.
+const paragraph = document.querySelector('.paragraph');
+
+console.log(paragraph);
+
+// Etape 2 : mettre en place un écouteur d'évènements
+paragraph.addEventListener('mouseover', () => {
+	// Etape 3 : Modifier l'élement ou son comportement
+	paragraph.style.fontSize = '20px';
+});
+
+paragraph.addEventListener('mouseout', () => {
+	paragraph.style.fontSize = '16px';
+});
+
+// --------------
+
+// Etape 1 - Selection
+const myForm = document.querySelector('#todoForm');
+const myInput = document.querySelector('#todo');
+const allTodos = document.querySelector('.all-todos');
+
+//Etape 2 - Paramétrage d'event
+
+myForm.addEventListener('submit', (event) => {
+	//Etape 3 - Modification
+	// Empecher le comportement par défaut du navigateur :
+	// envoyer le formulaire par rechargement de page
+	event.preventDefault();
+
+	// Creation d'un élement
+	const newTodo = document.createElement('p');
+	// Attribution d'un texte
+	newTodo.innerText = myInput.value;
+
+	const span = document.createElement('span');
+	span.textContent = 'X';
+	span.classList.add('suppr');
+	span.addEventListener('click', () => {
+		console.log(`Supprimer `);
+	});
+
+	newTodo.appendChild(span);
+
+	// Ajoute de l'élement aux enfants de allTodos
+	allTodos.appendChild(newTodo);
+
+	// Vide la valeur de l'input
+	myInput.value = '';
+});
